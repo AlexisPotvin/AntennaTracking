5905106953b665ab2cff8a07a5da9d0c47275e7d
AlexisPotvin <alexis.potvin.001@gmail.com>
2016-03-20 21:36:01 -0400

    Update Acc.py

--- Acc.py
+++ Acc.py
@@ -7,12 +7,13 @@ import os.path
 import time
 import math
 
+#IMU init
 k=0
 if k==0:
-	SETTINGS_FILE = "RTIMULib"
+	SETTINGS_FILE = "RTIMULib" #Init file name
 
 	print ("Using settings file" +  SETTINGS_FILE +  ".ini")
-
+	
 	if not os.path.exists(SETTINGS_FILE + ".ini"):
 
 		print("Settings file does not esist, will be created")
@@ -39,22 +40,22 @@ if k==0:
 	print("Recommended Poll Interval: %dmS\n" % poll_interval)
 
 	k=1
-
+#Read Imu number for X time and return the average value
 def ReadImu(accel,x):
 
 	r=0
 	p=0
 	y=0
 	i=0
-	#Faire une moyenne des x  données
+	#Collect IMU Data X times and calcue an average
 	while i < x :
 		if imu.IMURead():
 	
-			data = imu.getIMUData()
+			data = imu.getIMUData() # Read IMU data
 			fusionPose = data["fusionPose"]
 			"""print("r: %f p: %f y: %f" %(math.degrees(fusionPose[0]),
 			math.degrees(fusionPose[1]),math.degrees(fusionPose[2])))"""
-	
+			#Average calculation
 			r= r+ math.degrees(fusionPose[0])			
 			p= p+ math.degrees(fusionPose[1])
 			y= y+ math.degrees(fusionPose[2])
