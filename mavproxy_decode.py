2629f4e9cba6b6f1da21cc2cfffc8eb89fea42fd
AlexisPotvin <alexis.potvin.001@gmail.com>
2016-04-25 00:46:07 -0400

    Update mavproxy_decode.py

--- mavproxy_decode.py
+++ mavproxy_decode.py
@@ -2,7 +2,7 @@ __author__ = 'User'
 
 import json
 import threading
-import udp
+import socket
 
 class UAVgps(threading.Thread):
 
@@ -11,7 +11,11 @@ class UAVgps(threading.Thread):
         self.lat = 0
         self.alt = 0
         self.long = 0
-        self.data  = None
+        self.data = None
+        self.telemetryIP = 0
+        self.telemetryPort = 0
+        self.telemetrySocket = None
+
     def uav_altitude(self):
         return self.alt
 
@@ -21,12 +25,22 @@ class UAVgps(threading.Thread):
     def uav_longitude(self):
         return self.long
 
-    def decode_data(self, data):
+    def update_UAVgps(self):
         jsonStr = json.loads(data)
         if float(jsonStr['packet_id']) == 33:
             self.alt = float(jsonStr['alt'])
             self.lat = float(jsonStr['lat'])
             self.long = float(jsonStr['lon'])
 
+    def set_telemetry_IP (self, host_IP):
+        self.telemetryIP = host_IP
 
+    def set_telemetry_port(self, hostPort):
+        self.telemetryPort = hostPort
 
+    def create_bind_socket(self):
+        self.telemetrySocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
+        self.telemetrySocket.bind((self.telemetryIP, self.telemetryPort))
+
+    def recieve_telemetry(self):
+        self.data, addr = self.telemetrySocket.recvfrom(4096)
