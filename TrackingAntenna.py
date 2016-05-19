f2600a9edc926038b8f83cc4de4775c573125cdb
alexis.potvin <alexis.potvin.001@gmail.com>
2016-05-19 15:35:11 -0400

    IMU threaded

--- TrackingAntenna.py
+++ TrackingAntenna.py
@@ -1,6 +1,7 @@
 import Acc
 from antenna_class import Antenna
 from servo_class import NewServo
+from Acc_class import Accel
 import servo
 import time
 from keyboard import KBHit
@@ -8,16 +9,18 @@ from mavproxy_decode import UAVgps
 from GPS_thread import UartGPS
 
 
-
 #classkbhit contains actions relatives to the keyboard
 kb=KBHit()
 
 antenna = Antenna()
+antennaGps = UartGPS()
+Accel = Accel()
+
 #defien both the servos
 YawServo = NewServo(-180,180,1.1,1.9,1.5,100,0,3)
 PitchServo = NewServo(0,90,1.1,1.9,1.5,100,1,1)
 
-antennaGps = UartGPS()
+
 
 uav = UAVgps()
 uav.set_telemetry_IP("")
@@ -43,29 +46,39 @@ antenna.Orientationoffset(antenna.yaw)
 print "alt", antenna.antennaAlt, "lon:", antenna.antennaLon,"lat", antenna.antennaLat
 print "offset", antenna.bearingoffset
 
+
 while True:
-	print "antennaalt", antenna.antennaAlt	
-	uav.recieve_telemetry()
-    	uav.update_UAVgps()
-	uav.update_UAVAttitude()
-#	print 'uav lat', uav.lat, 'uavlong', uav.lon, 'uav.alt',uav.alt
-	#print 'uav', uav.roll
-    	#read current position
-	#Acc.emptyIMUBuff()
-	Acc.ReadImu(antenna,2)
-	#update th gps coordinate of the uav
-	antenna.uavLat = uav.lat
-	antenna.uavAlt = uav.alt
-	antenna.uavLon = uav.lon
-	#update the wanted angles
-	antenna.updateYawFromGPS()
-	antenna.updatePitchFromGPS()
-	#set the antenna to the correcte angle
-	#antenna.angleoffsetcalc()
-	#print 'antenna pitch', antenna.pitch
-	tickyaw=YawServo.Refresh(antenna.wyaw,antenna.yaw)
-	tickpitch=PitchServo.Refresh(antenna.wpitch,antenna.pitch)
-	print "yawtick",tickyaw, "Pitchticks", tickpitch
-	print "wyaw", antenna.wyaw, "yaw", antenna.yaw, "wpitch", antenna.wpitch , "pitch", antenna.pitch
-	print "new ite"
-#	time.sleep(0.1)
+	try:
+		print "antennaalt", antenna.antennaAlt
+		uav.recieve_telemetry()
+		uav.update_UAVgps()
+		uav.update_UAVAttitude()
+		#print 'uav lat', uav.lat, 'uavlong', uav.lon, 'uav.alt',uav.alt
+		#print 'uav', uav.roll
+
+		#read current position
+		Antenna.pitch = Accel.pitch
+		Antenna.yaw = Accel.yaw
+
+		#update th gps coordinate of the uav
+		antenna.uavLat = uav.lat
+		antenna.uavAlt = uav.alt
+		antenna.uavLon = uav.lon
+
+		#update the wanted angles
+		antenna.updateYawFromGPS()
+		antenna.updatePitchFromGPS()
+
+		#set the antenna to the correcte angle
+		#antenna.angleoffsetcalc()
+		#print 'antenna pitch', antenna.pitch
+		tickyaw=YawServo.Refresh(antenna.wyaw,antenna.yaw)
+		tickpitch=PitchServo.Refresh(antenna.wpitch,antenna.pitch)
+		print "yawtick",tickyaw, "Pitchticks", tickpitch
+		print "wyaw", antenna.wyaw, "yaw", antenna.yaw, "wpitch", antenna.wpitch , "pitch", antenna.pitch
+		print "new ite"
+		#time.sleep(0.1)
+
+	except (KeyboardInterrupt, SystemExit):
+		Accel.kill = True
+		raise
